120228700bf69647d1347579acd18bc45766d452
ibrhmhnfm <130213828+ibrhmhnfm@users.noreply.github.com>
2024-03-05 12:10:07 +0700

    Update testing1.c

--- testing1.c
+++ testing1.c
@@ -1,1 +1,11 @@
+/** EL2208 Praktikum Pemecahan Masalah dengan C 2023/2024
+ *   Modul               : 1 - Overview of C Language
+ *   Hari dan Tanggal    : Kamis, 29 Februari 2024
+ *   Nama (NIM)          : Ibrahim Hanif M (13222111)
+ *   Nama File           : main.c
+ *   Deskripsi           : Soal 1
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
